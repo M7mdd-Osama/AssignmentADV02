f4a7656623fd07cf077110fcdd5cf79bd8d185e5
Mohamed Osama Ali <ana20332183@gmail.com>
2024-08-05 02:39:27 +0300

    Array List

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Array List
+            //ArrayList arrayList = new ArrayList();
+            //arrayList => size 0
+            //arrayList => Capacity 0
+            //arrayList => Count 0
+
+            //Console.WriteLine($"Count = {arrayList.Count} , Capacity = {arrayList.Capacity}");
+
+            //arrayList.Add(1);
+            //arrayList.Add(2);
+            //arrayList.AddRange(new int[] {3,4});
+            //Console.WriteLine($"Count = {arrayList.Count} , Capacity = {arrayList.Capacity}");
+            //arrayList.Add(2);
+            //Console.WriteLine($"Count = {arrayList.Count} , Capacity = {arrayList.Capacity}");
+
+            //ArrayList arrayList2 = new ArrayList(5);
+            //arrayList2.AddRange(new int[] { 1, 2, 3, 4, 5, 6 });
+            //Console.WriteLine($"Count = {arrayList2.Count} , Capacity = {arrayList2.Capacity}");
+            //arrayList2.TrimToSize();
+            //Console.WriteLine($"Count = {arrayList2.Count} , Capacity = {arrayList2.Capacity}");
+
+            //arrayList2.Add("Mohamed"); //Heterogeneous List
+
+            //foreach(/*var*/int item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion        
 
         }
     }
