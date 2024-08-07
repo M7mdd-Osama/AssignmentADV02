59ee99b650dc4ba28c4e1a95d801da9476f59516
Mohamed Osama Ali <ana20332183@gmail.com>
2024-08-08 02:07:42 +0300

    Part01

---
+++ Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            //Write a Report about all Collections Given in the Session
+            //and Compare [Structure, Time Complexity, and Business Case With Implemented Examples]
+
+            //Array List: Resizable Array, Heterogeneous, Fast, Dynamic arrays with random access
+
+            //List: Abstract data type, Depends on implementation, Homogeneous, Defining a contract for list implementation
+
+            //Linked List: Nodes, Homogeneous, Dynamic Length, Slow, Frequent Insertions and deletions
+
+            //Stack: LIFO, Slow, Function call management, undo mechanisms
+
+            //Queue: FIFO, Slow, Task scheduling, handling requests in order
+            #endregion
+
+        }
+    }
+}
